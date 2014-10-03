745ed9f600464aaa4c879c99bb8d82e22c2b3546
Carolina <carolhmj@gmail.com>
2014-10-02 23:34:24 -0300

    Revert "Métodos"

--- src/métodos/metodosNumericos.h
+++
@@ -1,8 +0,0 @@
-#include "../funções/funcoesReais.h"
-#include <tuple>
-
-namespace tnw {
-	typedef std::tuple<double,double> intervalo;
-	tnw::intervalo bissec (tnw::intervalo a_b, FuncaoRealP f, double epsilon);
-	double pontoFixo(double inicial, FuncaoRealP phi, double epsilon);
-}
