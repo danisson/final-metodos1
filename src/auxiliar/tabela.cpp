763affa0ed8ede9e596e6de27e4a1923b2dd91ef
Heitor Oliveira <heitor@localhost.localdomain>
2014-10-26 19:13:25 -0300

    TraduzHunson

--- src/auxiliar/tabela.cpp
+++ src/auxiliar/tabela.cpp
@@ -1,27 +1,26 @@
 #include "tabela.h"
 
 
-/* Convert double to string with specified number of places after the decimal
-   and left padding. */
+/* Converte double pra string com número específico de espaços após a casa decimal 
+   e margem à esquerda */
 std::string tnw::prd(const double x, const int decDigits, const int width) {
     stringstream ss;
     ss << fixed << right;
-    ss.fill(' ');        // fill space around displayed #
-    ss.width(width);     // set  width around displayed #
-    ss.precision(decDigits); // set # places after decimal
-    ss << x;
+    ss.fill(' ');        
+    ss.width(width);     
+    ss.precision(decDigits);    ss << x;
     return ss.str();
 }
 
-/*! Center-aligns string within a field of width w. Pads with blank spaces
-    to enforce alignment. */
+/*! Alinha centralmente strings dentro de uma margem de largura w. Usa espaços em branco
+    para forçar alinhamento */
 std::string tnw::center(const string s, const int w) {
     stringstream ss, spaces;
-    int padding = w - s.size();                 // count excess room to pad
+    int padding = w - s.size();
     for(int i=0; i<padding/2; ++i)
         spaces << " ";
-    ss << spaces.str() << s << spaces.str();    // format with padding
-    if(padding>0 && padding%2!=0)               // if odd #, add 1 space
+    ss << spaces.str() << s << spaces.str();
+    if(padding>0 && padding%2!=0)
         ss << " ";
     return ss.str();
 }
