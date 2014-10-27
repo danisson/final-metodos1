71c6725360938f5ea5cdaec9592bcde8a9d2d7f0
Daniel Augusto <danisson10@gmail.com>
2014-10-27 14:14:41 -0300

    Adicionado segundo phi na comparação

--- src/aplicações/aplicacoes.cpp
+++ src/aplicações/aplicacoes.cpp
@@ -45,12 +45,12 @@ void tnw::desenha_quadro(std::vector<double> vetorP0, double epsilon){
 }
 
 void tnw::gerarQuadroComparativo(std::vector<double> p0,double epsilon,std::string nomeArquivo) {
-	tnw::FuncaoRealP f,phi;
+	tnw::FuncaoRealP f,phi,phi2;
 	tnw::FuncaoRealP raiz = newFun(tnw::FuncaoExistente(std::sqrt,"raiz"));
 	tnw::FuncaoRealP log = newFun(tnw::FuncaoExistente(std::log,"log"));
 	tnw::FuncaoRealP qd2 = newFun(tnw::Polinomio({0,0,4}));
 
-	tnw::outputMetodo saidas[3] = {tnw::outputMetodo(0,0),tnw::outputMetodo(0,0),tnw::outputMetodo(0,0)};
+	tnw::outputMetodo saidas[4] = {tnw::outputMetodo(0,0),tnw::outputMetodo(0,0),tnw::outputMetodo(0,0),tnw::outputMetodo(0,0)};
 	tnw::intervalo a_b;
 	double x0;
 
@@ -60,27 +60,30 @@ void tnw::gerarQuadroComparativo(std::vector<double> p0,double epsilon,std::stri
 		throw -1;
 
 	std::fprintf(arquivo,
-		"p0,a,b,phi,"
+		"p0,a,b,phi,phi2,"
 		"Método de Newton(d),Método de Newton(f(d)),Método de Newton(passos),"
 		"Método Modificado(d),Método Modificado(f(d)),Método Modificado(passos),"
-		"Ponto Fixo(d),Ponto Fixo(f(d)),Ponto Fixo(passos)"
+		"Ponto Fixo(d),Ponto Fixo(f(d)),Ponto Fixo(passos),"
+		"Ponto Fixo 2(d),Ponto Fixo 2(f(d)),Ponto Fixo 2(passos)"
 		"\n");
 	for (unsigned i = 0; i < p0.size(); ++i)
 	{
-		f   = newFun(tnw::Exponencial(p0[i])) - qd2;
-		phi = compose(log,qd2/p0[i]);
-		a_b = tnw::bissec(tnw::acharChuteInicialRandom(f),f,0.001);
-		x0  = (std::get<0>(a_b)+std::get<1>(a_b))/2.0;
+		f    = newFun(tnw::Exponencial(p0[i])) - qd2;
+		phi  = compose(log,qd2/p0[i]);
+		phi2 = compose(raiz,newFun(tnw::Exponencial(p0[i]/4.0)));
+		a_b  = tnw::bissec(tnw::acharChuteInicialRandom(f),f,0.001);
+		x0   = (std::get<0>(a_b)+std::get<1>(a_b))/2.0;
 
 		saidas[0] = tnw::newton(x0,f,epsilon);
 		saidas[1] = tnw::newtonModificado(x0,f,epsilon);
 		saidas[2] = tnw::pontoFixo(x0,phi,epsilon);
+		saidas[3] = tnw::pontoFixo(x0,phi2,epsilon);
 
 		std::fprintf(arquivo,
-		"%.*lf,%.*lf,%.*lf,%s,",
-		LDBL_DIG,p0[i],LDBL_DIG,std::get<0>(a_b),LDBL_DIG,std::get<1>(a_b),phi->toString().c_str());
+		"%.*lf,%.*lf,%.*lf,%s,%s,",
+		LDBL_DIG,p0[i],LDBL_DIG,std::get<0>(a_b),LDBL_DIG,std::get<1>(a_b),phi->toString().c_str(),phi2->toString().c_str());
 
-		for (int j = 0; j < 3; ++j)
+		for (int j = 0; j < 4; ++j)
 			std::fprintf(arquivo,"%.*lf,%.*le,%lld,",LDBL_DIG,saidas[j].x,LDBL_DIG,f->eval(saidas[j].x),saidas[j].i);
 		std::fprintf(arquivo,"\n");
 	}
